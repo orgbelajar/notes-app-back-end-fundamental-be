84e7c95638173a184b3af384d89480f9ccf56e46
nabbiil._ <nabilsyakir95@gmail.com>
2024-12-26 00:30:40 +0700

    postgres database implemented

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// mengimpor dotenv dan menjalankan konfigurasinya
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
-const NotesService = require('./services/inMemory/NotesService');
+const NotesService = require('./services/postgres/NoteService');
 const NotesValidator = require('./validator/notes');
 const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
@@ -29,7 +33,7 @@ const init = async () => {
     // mendapatkan konteks response dari request
     const { response } = request;
 
-    // penanganan client error secara internal.
+    // penanganan client error secara internal
     if (response instanceof ClientError) {
       const newResponse = h.response({
         status: 'fail',
