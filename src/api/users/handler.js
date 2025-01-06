4d8b002baf31216380a9152709dd5f952c0a02d0
nabbiil._ <nabilsyakir95@gmail.com>
2025-01-06 09:29:40 +0700

    user registration implemented

---
+++ src/api/users/handler.js
@@ -0,0 +1,43 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const autoBind = require('auto-bind');
+
+/* eslint-disable no-underscore-dangle */
+class UsersHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this); // mem-bind nilai this untuk seluruh method sekaligus
+  }
+
+  async postUserHandler(request, h) {
+    this._validator.validateUserPayload(request.payload);
+    const { username, password, fullname } = request.payload;
+
+    const userId = await this._service.addUser({ username, password, fullname });
+
+    const response = h.response({
+      status: 'success',
+      message: 'User berhasil ditambahkan',
+      data: {
+        userId,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async getUserByIdHandler(request, h) {
+    const { id } = request.params;
+    const user = await this._service.getUserById(id);
+
+    return {
+      status: 'success',
+      data: {
+        user,
+      },
+    };
+  }
+}
+
+module.exports = UsersHandler;
